9af1625ce6f3d68bfc0f16283f67f1c6e1b29f60
Andrew Landau <andrewtylerlandau@gmail.com>
2024-12-20 11:44:32 +0000

    update version (and support for tests)

--- conditional_parser.py
+++ conditional_parser.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 from inspect import signature
 from argparse import ArgumentParser, Namespace
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 class ConditionalArgumentParser(ArgumentParser):
